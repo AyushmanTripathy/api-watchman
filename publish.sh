ebf1faaf03608b80b253860bb094a766ade9fb61
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2021-10-15 19:23:10 +0530

    publishing

--- publish.sh
+++ publish.sh
@@ -1,11 +1,9 @@
-git add .
-git commit -m 'publishing'
-
-cd ~/api-watchman
+#!/bin/sh
 
 echo "building"
 npm run build
 
+git commit -am 'publishing'
 npm version minor
 
 echo "publishing"
@@ -14,4 +12,19 @@ npm publish
 git add .
 git commit -m "published $(npm view psre version)"
 git push origin master
+echo 'successfully published'
+
+git push origin master
+
+# merging release branch
+echo "merging master --> release"
+curl \
+  -X POST \
+  -u "AyushmanTripathy":$(cat ~/.pat) \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/repos/AyushmanTripathy/pipe-script/merges" \
+  -d '{"base":"release","head":"master"}'
+echo "merge complete"
+
+git commit -am "published $(npm view psre version)"
 echo 'successfully published'
